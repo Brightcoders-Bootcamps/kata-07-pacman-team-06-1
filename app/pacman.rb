d50247375d051c938dab7a6daebd9a8f3e2fcb54
ferreirarauldev <ferreirarauldev@gmail.com>
2021-01-20 19:56:07 -0300

    i refactor pacman class with OOP
    rf

--- app/pacman.rb
+++ app/pacman.rb
@@ -1,119 +1,38 @@
-require 'ruby2d'
-
-@pacman = Circle.new( x: 200 , y: 100, radius: 15, sectors: 32, color: 'yellow', z: 10 )
-
-
-@x_speed = 0
-@y_speed = 0
-places = Array.new
-score = 0
-
-
-on :key_down do |event|
-  if event.key == 'a' || event.key == 'left' 
-    @x_speed = -1
-    @y_speed = 0
-  elsif event.key == 'd' || event.key == 'right' 
-    @x_speed = 1
-    @y_speed = 0
-  elsif event.key == 'w' || event.key == 'up' 
-    @x_speed = 0
-    @y_speed = -1
-  elsif event.key == 's' || event.key == 'down' 
-    @x_speed = 0
-    @y_speed = 1
+class Pacman 
+  attr_accessor :x, :y
+  def initialize(x, y)
+      @x_speed = 0
+      @y_speed = 0
+      @x = x
+      @y = y
+      @pacman = Circle.new( x: x , y: y, radius: 15, sectors: 32, color: 'yellow', z: 10 )
+  end
+  
+  def get_coordinates
+      [self.x, self.y]
   end
 
-end
-
-score_text = Text.new(
-  "%03d" % score,
-  x: 530,
-  y: 10,
-  color: 'white',
-  size: 20,
-  opacity: 1
-)
-
-update do
-	puts @pacman.x
-	if (@pacman.x + @x_speed).between?(500, 580)
-		@pacman.x = @pacman.x
-	else
-		@pacman.x += @x_speed
-	end
-  @pacman.y += @y_speed
-  fruit = places.select {|fruit| fruit.x.between?(@pacman.x - 20, @pacman.x + 20) && fruit.y.between?(@pacman.y - 20, @pacman.y + 20)}
-  unless fruit[0].class  == NilClass
-  	 fruit[0].remove; score_text.text = "Score: #{score+=1 }"
-  	 places.delete(fruit[0])
+  def move_x(x)
+      @pacman.x += (x + @x_speed)
   end
- 
 
+  def move_y(y)
+      @pacman.y += (y + @y_speed)
+  end
+
+  def check_colision
+      @pacman.contains?(object.first.x, object.first.y)
+  end
+
+  private
+  def get_object(x, y, array_objects)
+
+  end
+
+  def eat_fruit(fruit, array_objects)
+      fruit.remove
+      array_objects.delete(fruit)
+  end
+
+  
 end
-
-
-
-#paredes inicio
-Square.new(
-  x: 500, y: 80,
-  size: 80,
-  color: 'blue',
-  z: 20
-)
-
-Square.new(
-  x: 300, y: 80,
-  size: 80,
-  color: 'blue',
-  z: 20
-)
-
-Square.new(
-  x: 100, y: 80,
-  size: 80,
-  color: 'blue',
-  z: 20
-)
-
-Line.new(
-  x1: 90, y1: 265,
-  x2: 550, y2: 265,
-  width: 25,
-  color: 'lime',
-  z: 20
-)
-
-Square.new(
-  x: 500, y: 360,
-  size: 80,
-  color: 'blue',
-  z: 20
-)
-
-Square.new(
-  x: 300, y: 360,
-  size: 80,
-  color: 'blue',
-  z: 20
-)
-
-Square.new(
-  x: 100, y: 360,
-  size: 80,
-  color: 'blue',
-  z: 20
-)
-
-#paredes fin
-
-#
-(50..get(:height)).each do |height|
-	(50..get(:width)).each do |width|
-		 if width % 25 == 0 && height % 25 == 0
-		 	places << Circle.new( x: width , y: height, radius: 4, sectors: 32, color: 'orange', z: 10 )
-		 end
-	end
-end
-
-show
