a237eaaef3107ebf7b15173085126e8a6310c402
Jose Alberto Negrete Gonzalez <jnegrete15@ucol.mx>
2021-01-20 16:39:32 -0600

    World and wall class modified.

--- app/wall.rb
+++ app/wall.rb
@@ -1,17 +1,25 @@
 class Wall
-    def initialize()
-    end
+    attr_reader @wall2d
 
-    #crear objetos
-    def create(x, y, color)
-    
-    end
+    def initialize(x, y)
+        @x1_position = x
+        @y1_position = y
+     
+
+        @wall2d = Square.new(x: x, y: y,size: 30,color: 'blue',z: 10)
+    end    
+
+    wall = Wall.new 
+    puts wall.class # class Wall 
+
+
 
     # eliminar
     def remove
-        self.remove
+        @wall2d.remove
     end
-    # retornar un array con x e y
+
+    # retornar un array con x, y
     def get_coordinates
     end
 end
--- app/world.rb
+++ app/world.rb
@@ -1,13 +1,16 @@
+require 'ruby2d'
 class World
     def initialize
         create_world("Pacman Game")
     end
 
     def create_world(title)
-        set title: title,
+        Window.set title: title,
         background: 'black',
         width: 640,
         height: 480,
         resizable: false
+        Window.show
     end
 end
+World.new
