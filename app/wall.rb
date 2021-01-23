b66ceeb60975f4940ea1ed4e8568e0d265a5b5c8
ferreirarauldev <ferreirarauldev@gmail.com>
2021-01-23 19:40:40 -0300

    refactor class

--- app/wall.rb
+++ app/wall.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
+# class for square inside windows
 class Wall
   attr_reader :wall2d, :x, :y, :width, :height
 
-  def initialize(x, y)
-      @wall2d = Square.new(x: x, y: y,size: 70,color: 'blue',z: 10)
-      @x = x
-      @y = y
-      @width = @wall2d.width
-      @height = @wall2d.height
-  end    
+  def initialize(coord_x, coord_y)
+    @wall2d = Square.new(x: @x, y: @y, size: 70, color: 'blue', z: 10)
+    @x = coord_x
+    @y = coord_y
+    @width = @wall2d.width
+    @height = @wall2d.height
+  end
 end
