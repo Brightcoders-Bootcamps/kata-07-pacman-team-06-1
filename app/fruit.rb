5c6fad40523e87e723fa3a86867b722f8c134b28
ferreirarauldev <ferreirarauldev@gmail.com>
2021-01-23 19:57:16 -0300

    apply rubocop

--- app/fruit.rb
+++ app/fruit.rb
@@ -3,15 +3,11 @@
 # Class that describe the fruit object
 class Fruit
   attr_reader :x, :y, :fruit, :radius
-  def initialize(x, y)
-      @x = x
-      @y = y
-      @fruit = Circle.new( x: x , y: y, radius: 4, color: 'orange', z: 1 )
-      @radius = @fruit.radius
-  end
 
-  def get_coordinates
-      return [self.x, self.y]
+  def initialize(coord_x, coord_y)
+    @x = coord_x
+    @y = coord_y
+    @fruit = Circle.new(x: @x, y: @y, radius: 4, color: 'orange', z: 1)
+    @radius = @fruit.radius
   end
-
 end
