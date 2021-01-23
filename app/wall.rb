009d75d9365fd4d77048acb3188ece90ceca84c6
ferreirarauldev <ferreirarauldev@gmail.com>
2021-01-23 19:58:16 -0300

    fix issue with coordinates variables

--- app/wall.rb
+++ app/wall.rb
@@ -5,7 +5,7 @@ class Wall
   attr_reader :wall2d, :x, :y, :width, :height
 
   def initialize(coord_x, coord_y)
-    @wall2d = Square.new(x: @x, y: @y, size: 70, color: 'blue', z: 10)
+    @wall2d = Square.new(x: coord_x, y: coord_y, size: 70, color: 'blue', z: 10)
     @x = coord_x
     @y = coord_y
     @width = @wall2d.width
